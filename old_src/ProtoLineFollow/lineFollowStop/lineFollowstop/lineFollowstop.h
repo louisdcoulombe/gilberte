f7664e2b9e4b58832d9930d8fdbbf5c7d3027627
elwiss <louisd.coulombe@gmail.com>
2016-04-11 19:17:42 -0400

    Moved sources to cleaner repo

---
+++ old_src/ProtoLineFollow/lineFollowStop/lineFollowstop/lineFollowstop.h
@@ -0,0 +1,33 @@
+//robotState
+typedef enum  {
+  STATEIDLE,      //Wait command
+  STATEFOLLOW,   //follow line
+  STATETARGETFOUND,  //Target is Found need to stop
+  STATETARGETPASSED, //Target Passed, need to backup
+  STATEONTARGET,
+  STATETARGETPOS,
+  STATERIGHTON,
+  STATEFOLLOWNOSTOP,
+  STATEFINDLINEONTURN
+}robotState;
+
+#define NOERROR 0
+#define ERRORCALIBRATION 0xEC
+#define CALLOOPREAD 30
+#define CALIBRATIONSPEED 125
+#define ANGLETORUNCAL  PI/6
+
+#define LINEREFRESH 25
+#define WHEELREFRESH 2
+
+//Internal wheelspan : 200mm
+//External wheelspan : 280mm
+//WhellDiameter : 106mm
+//GearRatio : 1:64
+#define TANKWHEELDIA 106
+#define TANKWHEELSPAN 450
+
+#define NUM_SENSORS   8     // number of sensors used
+#define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
+#define EMITTER_PIN   13     // emitter is controlled by digital pin 2
+#define QTRTHRESHOLD  800
